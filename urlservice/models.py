0427d781270058dc5ab8972392d8577070e7ba2f
abhikumar22 <abhi.ckp1002@gmail.com>
2020-03-23 18:03:42 +0530

    upgraded length

--- urlservice/models.py
+++ urlservice/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Urlsortner(models.Model):
-    full_url = models.CharField(max_length=100)
+    full_url = models.CharField(max_length=1000)
     sort_url = models.CharField(max_length=30)
     no_of_clicks = models.CharField(max_length=30)
